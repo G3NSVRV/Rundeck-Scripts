59c3d546f1908aac85784dcb54ff445956aef34a
[G3NSVRV] <11720535+G3NSVRV@users.noreply.github.com>
2020-11-11 17:16:18 -0300

    Update user-pass-login.sh
    delete unnecessary variables

--- Scripts/user-pass-login.sh
+++ Scripts/user-pass-login.sh
@@ -3,8 +3,5 @@
 rundeckServer="http://node01.rundeck.local:4440"
 rundeckUser="user1"
 rundeckPass="user"
-rundeckApiversion="34"
-contentFormat="application/json"
-projectName="Project"
 
 curl -v -X "POST" -d "j_username=$rundeckUser" -d "j_password=$rundeckPass" -c cookie -b cookie "$rundeckServer"/j_security_check
