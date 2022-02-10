9e97c0377e7952dff3256ff4c264172a425e31d8
G3NSVRV <11720535+G3NSVRV@users.noreply.github.com>
2022-02-10 15:46:59 -0300

    updating files

--- Scripts/user-pass-login.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rundeckServer="http://node01.rundeck.local:4440"
-rundeckUser="user1"
-rundeckPass="user"
-
-curl -v -X "POST" -d "j_username=$rundeckUser" -d "j_password=$rundeckPass" -c cookie -b cookie "$rundeckServer"/j_security_check
