10ad9ebb043537929e8fc2340bbda9a07dca55e6
G3NSVRV <11720535+G3NSVRV@users.noreply.github.com>
2022-11-28 20:11:45 -0300

    modify headers

--- Scripts/bulk/import_projects/importProjects.sh
+++ Scripts/bulk/import_projects/importProjects.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-#rundeckServer="http://tam-test-95976867.us-east-2.elb.amazonaws.com/rundeck"
-#rundeckToken="LMfHOAodAtZTQo45z1Ws6viUjkkLMaz4"
-#apiVersion="38"
-#cookie="cookie"
-#curlOpt="-s"
-#apiFormat="application/json"
-#jobFormat="application/yaml"
-#projectPrefix="jgarces"
-
 source config
 
 for n in $(eval echo "{1..$amountProjects}")
