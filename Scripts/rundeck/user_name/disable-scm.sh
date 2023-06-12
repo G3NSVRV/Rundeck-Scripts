e603667e514130a9e016e6c2a20ca15b52f3ce9c
G3NSVRV <11720535+G3NSVRV@users.noreply.github.com>
2023-06-12 12:58:05 -0400

    new implementation of docker-compose

--- Scripts/rundeck/user_name/disable-scm.sh
+++ Scripts/rundeck/user_name/disable-scm.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #############################################
-echo "Rundeck Server hostname:"; read -r rundeckServer
-echo -e "\nRundeck username:"; read -r rundeckUser
-echo -e "\nRundeck password:"; read -r rundeckPass
+# echo "Rundeck Server hostname:"; read -r rundeckServer
+# echo -e "\nRundeck username:"; read -r rundeckUser
+# echo -e "\nRundeck password:"; read -r rundeckPass
 #############################################
 # to run unatended mode, please comment the code above and uncomment the code below, and replace with your props
 #############################################
-# rundeckServer="http://localhost:4440"
-# rundeckUser="admin"
-# rundeckPass="admin"
+rundeckServer="http://node01-linux.local:4440"
+rundeckUser="admin"
+rundeckPass="admin"
 #############################################
 # Options Variables
 #############################################
@@ -17,15 +17,23 @@ cookie="cookie"
 #############################################
 rundeckApiVersion="34"
 rundeckApiFormat="json"
-rundeckProjectFormat="json"
 #############################################
 
 curl "$curlOptions" -X "POST" -d "j_username=$rundeckUser" -d "j_password=$rundeckPass" -c "$cookie" "$rundeckServer"/j_security_check
 
 for projectName in $(curl "$curlOptions" -X "GET" -H "Accept: application/$rundeckApiFormat" -H "Content-Type: application/$rundeckApiFormat" -c "$cookie" -b "$cookie" "$rundeckServer"/api/"$rundeckApiVersion"/projects|sed 's/,/\n/g'| grep name | cut -d ":" -f2 | tr -d '"')
 do
-curl "$curlOptions" -X "GET" -H "Accept: application/$rundeckProjectFormat" -H "Content-Type: application/$rundeckProjectFormat" -c "$cookie" -b "$cookie" "$rundeckServer"/api/"$rundeckApiVersion"/project/"$projectName"/scm/import/plugins
-curl "$curlOptions" -X "GET" -H "Accept: application/$rundeckProjectFormat" -H "Content-Type: application/$rundeckProjectFormat" -c "$cookie" -b "$cookie" "$rundeckServer"/api/"$rundeckApiVersion"/project/"$projectName"/scm/export/plugins
+scmImport=$(curl "$curlOptions" -X "GET" -H "Accept: application/$rundeckApiFormat" -H "Content-Type: application/$rundeckApiFormat" -c "$cookie" -b "$cookie" "$rundeckServer"/api/"$rundeckApiVersion"/project/"$projectName"/scm/import/plugins | jq '{"type":.plugins[].type,"enabled":.plugins[].enabled}')
+scmExport=$(curl "$curlOptions" -X "GET" -H "Accept: application/$rundeckApiFormat" -H "Content-Type: application/$rundeckApiFormat" -c "$cookie" -b "$cookie" "$rundeckServer"/api/"$rundeckApiVersion"/project/"$projectName"/scm/export/plugins | jq '{"type":.plugins[].type,"enabled":.plugins[].enabled}')
+
+if [ -n "$(echo $scmImport | grep -o '"enabled": true')" ]
+then
+echo SCM Import Works
+fi
+if [ -n "$(echo $scmExport | grep -o '"enabled": true')" ]
+then
+echo SCM Export Works
+fi
 done
 
 rm -f "$cookie"
